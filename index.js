32a81be39ac9a2cd35c7d051298b4f16bf79c1ac
AbdulSami-07 <sami07israil@gmail.com>
2020-11-25 19:13:09 +0530

    Mongoose Part 2

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
+//connect to mongodb server
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: "Uthappizza",
         description : "test"
-    });
-
-    newDish.save()
+    })
         .then((dish) =>{
             console.log(dish);
 
